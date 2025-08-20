de36229e6982283892ea3780489bc11fc55659b3
bibchamp <bibliotheque.champborne@gmail.com>
2025-08-20 15:12:53 +0400

    Update matchid-proxy.js

--- netlify-functions/matchid-proxy.js
+++ netlify-functions/matchid-proxy.js
@@ -5,7 +5,7 @@ exports.handler = async function(event, context) {
   const from = event.queryStringParameters.from || 0;
   const size = event.queryStringParameters.size || 10;
 
-  const apiKey = "VOTRE_CLE_API_ICI";
+  const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiaWRyaXNzLmJhdG91QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiNDMwNTI1Iiwic2NvcGVzIjpbInVzZXIiXSwiaWF0IjoxNzU1NjgyMjY4LCJleHAiOjE3NTgyNzQyNjgsImp0aSI6IjE3NTU2ODIyNjgifQ.4vpLvF6u0ZDHVEf-QzL3cZUcFDlsYl5SnOp7tmFLt_E";
 
   const url =
     `https://deces.matchid.io/deces/api/v1/search?q=${encodeURIComponent(query)}&from=${from}&size=${size}`;
